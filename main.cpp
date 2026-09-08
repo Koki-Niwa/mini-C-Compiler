c96be0ca8c6e6e2fdc5c849fccd72a9626ae95b2
Koki-Niwa <2390359880@qq.com>
2026-09-08 16:49:25 +0800

    feat:simple add sub

--- main.cpp
+++ main.cpp
@@ -11,13 +11,25 @@ int main(int argc,char* argv[]){
     std::string input_str=argv[1];
     char* end_ptr=nullptr;
     long value =std::strtol(input_str.c_str(),&end_ptr,10);
-    if(*end_ptr!='\0'){
-        std::cerr<<"The input parameter is not purely numeric."<<std::endl;
-        return EXIT_FAILURE;
-    }
     std::cout<<".intel_syntax noprefix"<<std::endl;
     std::cout<<".globl main"<<std::endl;
     std::cout << "main:"<<std::endl;
-    std::cout<<" mov rax,"<<value<<std::endl;
-    std::cout<<" ret"<<std::endl;
+    std::cout<<"    mov rax,"<<value<<std::endl;
+    while(*end_ptr){
+        if(*end_ptr=='+'){
+            end_ptr++;
+            value =std::strtol(end_ptr,&end_ptr,10);
+            std::cout<<"    add rax,"<<value<<std::endl;
+            continue;
+        }
+        else if(*end_ptr=='-'){
+            end_ptr++;
+            value =std::strtol(end_ptr,&end_ptr,10);
+            std::cout<<"    sub rax,"<<value<<std::endl;
+            continue;
+        } 
+        std::cerr<<"invalid input"<<std::endl;
+        return EXIT_FAILURE;
+        }
+    std::cout<<"    ret"<<std::endl;
 }
--- test.sh
+++ test.sh
@@ -16,7 +16,9 @@ assert() {
   fi
 }
 
-assert 0 0
-assert 42 42
+assert 0 10-10
+assert 42 30+10+2
+assert 0 0afad 
+assert 0 1+2-3+4-4
 
 echo OK
